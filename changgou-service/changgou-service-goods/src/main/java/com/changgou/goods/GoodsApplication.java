a7219dad1da107ff18270f4122e0a55d36f77d98
yangyongqian <qian.dareway@gmail.com>
2020-08-13 00:34:55 +0800

    宿舍提交

--- changgou-service/changgou-service-goods/src/main/java/com/changgou/goods/GoodsApplication.java
+++ changgou-service/changgou-service-goods/src/main/java/com/changgou/goods/GoodsApplication.java
@@ -6,11 +6,11 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableEurekaClient  // 开启Eureka 客户端
 /*  @MapperScan是tk.mybatis.spring.annotation包下的，用于扫描Mapper接口 */
 @MapperScan(basePackages = {"com.changgou.goods.dao"})
 public class GoodsApplication {
     public static void main(String[] args) {
-        SpringApplication.run(GoodsApplication.class);
+        SpringApplication.run(GoodsApplication.class,args);
     }
 }
